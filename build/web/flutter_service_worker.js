e51b565e00f0721a2d17ac02b8e4bd448b7abf05
Josh Chan <jfusion3600@gmail.com>
2024-01-21 23:33:28 +1100

    Cloned main repo

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "64e9da82f2caa68862eb002596e4462d",
-"index.html": "a76161e09a8f43c3e9a30137e2481c7b",
-"/": "a76161e09a8f43c3e9a30137e2481c7b",
-"main.dart.js": "e2bf323168439be77b6c734eb90fda89",
+"index.html": "b7c4d3be58d50515ea925ddefe60a356",
+"/": "b7c4d3be58d50515ea925ddefe60a356",
+"main.dart.js": "8a5bcb1aedc618f5503a51361d7c8aab",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
